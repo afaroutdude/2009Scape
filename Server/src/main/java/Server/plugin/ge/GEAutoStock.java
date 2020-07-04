d9e2371abd66bdd5358a5424f875d8525ef737e9
Adam Farabaugh <adamfara@seas.upenn.edu>
2020-07-04 16:42:13 -0700

    added some input checking to GEAutoStock

--- Server/src/main/java/Server/plugin/ge/GEAutoStock.java
+++ Server/src/main/java/Server/plugin/ge/GEAutoStock.java
@@ -10,62 +10,63 @@ import core.game.node.entity.player.info.PlayerDetails;
 import core.game.node.item.Item;
 
 /**
- * Auto stocking feature for the grand exchange. 
- * @author jamix77
+ * Auto stocking feature for the grand exchange.
  *
+ * @author jamix77
  */
 public class GEAutoStock {
-	
-	/**
-	 * Should the auto stock feature be enabled in the game?
-	 */
-	public static final boolean toStock = true;
-	
-	/**
-	 * The items to auto stock in the grand exchange.
-	 */
-	public static final ArrayList<Item> itemsToStock = new ArrayList<Item>();
-	
-	/**
-	 * The values to modify the price by
-	 */
-	public static final ArrayList<Double> modValues = new ArrayList<Double>();
-	
-	/**
-	 * Time to auto stock.
-	 */
-	public static void stock() {
-		if (itemsToStock.isEmpty()) {
-			try {
-			      File file = new File("data/eco/itemstostock.txt");
-			      Scanner myReader = new Scanner(file);
-			      while (myReader.hasNextLine()) {
-			        String data = myReader.nextLine();
-			        if (data.startsWith("#")) {
-			        	continue;
-			        }
-			        itemsToStock.add(new Item(Integer.parseInt(data.split(":")[0]), Integer.parseInt(data.split(":")[1])));
-			        modValues.add(Double.parseDouble(data.split(":")[2]));
-			      }
-			      myReader.close();
-			    } catch (FileNotFoundException e) {
-			      System.out.println("An error occurred.");
-			      e.printStackTrace();
-			    }
-;		}
-		
-		
-		for (int i = 0; i < itemsToStock.size(); i++) {
-			Item item = itemsToStock.get(i);
-			double markup = modValues.get(i);
-			System.out.println("Stocking " + item.getName() + " id: " + item.getId() + " for " + (int)((item.getValue() * markup)/item.getAmount()) + " x" + item.getAmount() + " at " + markup + "x markup");
-			GrandExchangeOffer offer = new GrandExchangeOffer(item.getId(), true);
-			offer.setOfferedValue((int) ((item.getValue() * markup)/ item.getAmount()));
-			offer.setAmount(item.getAmount());
-			System.out.println(GEOfferDispatch.dispatch(new Player(PlayerDetails.getDetails("2009scape")), offer));
-		}
-	}
-	
-	
-	
+
+    /**
+     * Should the auto stock feature be enabled in the game?
+     */
+    public static final boolean toStock = true;
+
+    /**
+     * The items to auto stock in the grand exchange.
+     */
+    public static final ArrayList<Item> itemsToStock = new ArrayList<Item>();
+
+    /**
+     * The values to modify the price by
+     */
+    public static final ArrayList<Double> modValues = new ArrayList<Double>();
+
+    /**
+     * Time to auto stock.
+     */
+    public static void stock() {
+        if (itemsToStock.isEmpty()) {
+            try {
+                File file = new File("data/eco/itemstostock.txt");
+                Scanner myReader = new Scanner(file);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine().split("#")[0];
+                    if (data != null && !data.isEmpty()) {
+                        if (data.split(":") != null && data.split(":").length == 3) {
+                            itemsToStock.add(new Item(Integer.parseInt(data.split(":")[0]), Integer.parseInt(data.split(":")[1])));
+                            modValues.add(Double.parseDouble(data.split(":")[2]));
+                        }
+                    }
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            ;
+        }
+
+
+        for (int i = 0; i < itemsToStock.size(); i++) {
+            Item item = itemsToStock.get(i);
+            double markup = modValues.get(i);
+            System.out.println("Stocking " + item.getName() + " id: " + item.getId() + " for " + (int) ((item.getValue() * markup) / item.getAmount()) + " x" + item.getAmount() + " at " + markup + "x markup");
+            GrandExchangeOffer offer = new GrandExchangeOffer(item.getId(), true);
+            offer.setOfferedValue((int) ((item.getValue() * markup) / item.getAmount()));
+            offer.setAmount(item.getAmount());
+            System.out.println(GEOfferDispatch.dispatch(new Player(PlayerDetails.getDetails("2009scape")), offer));
+        }
+    }
+
+
 }
